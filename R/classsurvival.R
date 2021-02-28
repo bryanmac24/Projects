182e884739f98ff5ccbd2fa946ce5fe81a554084
bryanmac24 <bryanmac.24@gmail.com>
2021-02-27 17:58:01 -0800

    Add files via upload

---
+++ R/classsurvival.R
@@ -0,0 +1,69 @@
+library(dplyr)
+library(caret)
+library(mlbench)
+library(MLeval)
+
+data<-coronavirus::coronavirus %>%
+  filter(!type == "confirmed") %>%
+  filter(!is.na(type))
+
+intrain <- createDataPartition(y = data$type, p= 0.8, list = FALSE)
+training <- data[intrain,]
+testing <- data[-intrain,]
+
+training[["type"]] = factor(training[["type"]])
+
+# prepare training scheme
+control <- trainControl(method = "cv", 
+                        number = 10, 
+                        savePredictions = TRUE, 
+                        classProbs = TRUE, 
+                        verboseIter = TRUE)
+# CART
+fit.cart <- train(type ~ cases, data=training, method="rpart", trControl=control, preProc=c("center", "scale"))
+# LDA
+fit.lda <- train(type ~ cases, data=training, method="lda", trControl=control, preProc=c("center", "scale"))
+# SVM
+fit.svm <- train(type ~ cases, data=training, method="svmRadial", trControl=control, preProc=c("center", "scale"))
+# kNN
+fit.knn <- train(type ~ cases, data=training, method="knn", trControl=control, preProc=c("center", "scale"))
+# Random Forest
+fit.rf <- train(type ~ cases, data=training, method="rf", trControl=control, preProc=c("center", "scale"))
+# Logistic Regression
+fit.log <- train(type ~ cases, data=training, method="glm", trControl=control,family=binomial, preProc=c("center", "scale"))
+# Neural Net
+fit.nn <- train(type ~ cases, data=training, method="nnet", trControl=control, preProc=c("center", "scale"))
+# collect resample
+results <- resamples(list(CART=fit.cart, LDA=fit.lda, SVM=fit.svm, KNN=fit.knn, RF=fit.rf,Log.Regression=fit.log,NN=fit.nn))
+
+# summarize differences between modes
+summary(results)
+
+#box and whisker plots to compare models
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+bwplot(results, scales=scales)
+
+res <- evalm(list(fit.cart,fit.lda,fit.svm,fit.knn,fit.rf,fit.log,fit.nn),gnames=c('CART','LDA','SVM','KNN','RF','LOG.REGRESSION','NN'), rlinethick=0.8, fsize=8, plots='r')
+
+# Cox Survival
+library(survival)
+cox <- coxph(Surv(date, deaths) ~ workplace_closing + testing_policy + information_campaigns + international_movement_restrictions
+             + internal_movement_restrictions + school_closing + cancel_events + gatherings_restrictions + transport_closing + stay_home_restrictions +
+               contact_tracing, data = covid)
+summary(cox)
+
+covid <-COVID19::covid19()
+
+covid$date <- as.numeric(as.POSIXct(covid$date, format="%m-%d-%Y"))
+
+str(covid)
+
+covid <- covid %>% filter(id == "USA")
+
+covid[is.na(covid)] <- 0
+
+library(survival)
+cox <- lm(deaths ~ workplace_closing + testing_policy + information_campaigns + international_movement_restrictions
+          + internal_movement_restrictions + school_closing + cancel_events + gatherings_restrictions + transport_closing + stay_home_restrictions +
+            contact_tracing, data = covid)
+summary(cox)
